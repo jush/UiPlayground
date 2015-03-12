338f83dfa9173666904c68773b1d124a9e07c148
Ramon <oni.pool@gmail.com>
2015-03-12 13:20:19 +0200

    Divided main view into two and better effect

--- app/src/main/java/org/jush/uiplayground/RotatePageTransformer.java
+++ app/src/main/java/org/jush/uiplayground/RotatePageTransformer.java
@@ -5,8 +5,8 @@ import android.util.Log;
 import android.view.View;
 
 public class RotatePageTransformer implements ViewPager.PageTransformer {
-    private static final float MIN_SCALE = 0.85f;
-    private static final float MIN_ALPHA = 0.5f;
+    private static final float MIN_SCALE = 0;
+    private static final float MIN_ALPHA = 0f;
 
     public void transformPage(View view, float position) {
         int pageWidth = view.getWidth();
@@ -15,7 +15,7 @@ public class RotatePageTransformer implements ViewPager.PageTransformer {
                 "height: %d", view
                 .toString(), position, pageWidth, pageHeight));
 //        view.setPivotX(0);
-//        view.setPivotY(pageHeight / 2);
+        view.setPivotY(pageHeight);
 
         if (position <= -1) { // [-Infinity,-1]
             // This page is way off-screen to the left.
@@ -24,19 +24,8 @@ public class RotatePageTransformer implements ViewPager.PageTransformer {
         } else if (position < 1) { // (-1,1)
             // Modify the default slide transition to shrink the page as well
             float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-            //            if (position < 0) {
-            //                view.setTranslationX(horzMargin - vertMargin / 2);
-            //            } else {
-            //                view.setTranslationX(-horzMargin + vertMargin / 2);
-            //            }
-
-            // Scale the page down (between MIN_SCALE and 1)
-            //            view.setScaleX(scaleFactor);
-            //            view.setScaleY(scaleFactor);
             view.setTranslationX((-position) * pageWidth); //shift the view over
-            view.setRotation(90 * position);
+            view.setRotation(60 * position);
 
             // Fade the page relative to its size.
             view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 -
